54c6ba46be727049da1ae4bd25cad25447f5282e
behzodjon <khudoyberdievgates@gmail.com>
2018-12-01 14:54:06 +0500

    first commit1

--- resources/views/main/listing.blade.php
+++ resources/views/main/listing.blade.php
@@ -181,7 +181,7 @@
 
                                         </ul>
                                         <div class="bottom-icons">
-                                            <div class="closed-now">CLOSED NOW</div>
+                                            <div class="closed-now">CLOSE NOW</div>
                                             <span class="ti-heart"></span>
                                             <span class="ti-bookmark"></span>
                                         </div>
